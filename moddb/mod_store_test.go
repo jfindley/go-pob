eb3905cc6eaa9d2947e4f0e1f52ba7321d54aff4
James Findley <jfindley@users.noreply.github.com>
2024-12-06 02:04:40 +0000

    fix: add initial moddb tests (#9)
    * chore: update to stdlib slog and slices
    
    * fix assert order
    
    * use stdlib copy function
    
    * unexport internal funcs
    
    * add tests to mod_list and fix a logic bug
    
    * add tests for Override
    
    * add initial mod_store tests
    
    * fix a couple lint errors

---
+++ moddb/mod_store_test.go
@@ -0,0 +1,64 @@
+package moddb
+
+import (
+	"testing"
+
+	"github.com/MarvinJWendt/testza"
+	"github.com/Vilsol/go-pob/mod"
+	"github.com/Vilsol/go-pob/utils"
+)
+
+func TestClone(t *testing.T) {
+	m := NewModStore(nil)
+	m.Conditions["test condition"] = true
+	m.Multipliers["test multiplier"] = 42.0
+	r := m.Clone()
+	testza.AssertEqual(t, m, r)
+}
+
+func TestMultiplier(t *testing.T) {
+	tc := []struct {
+		name        string
+		mod         mod.Mod
+		storeMods   []mod.Mod
+		cfg         *ListCfg
+		tag         *mod.MultiplierTag
+		multipliers map[string]float64
+		expected    float64
+	}{
+		{
+			name: "single mod",
+			mod:  mod.NewFloat("testMod0", mod.TypeIncrease, 10),
+			tag: &mod.MultiplierTag{
+				Division:     5,
+				VariableList: []string{"FullLife"},
+			},
+			multipliers: map[string]float64{"FullLife": 200},
+			expected:    400,
+		},
+		{
+			name: "limited mod",
+			mod:  mod.NewFloat("testMod0", mod.TypeIncrease, 10),
+			tag: &mod.MultiplierTag{
+				Division:         5,
+				VariableList:     []string{"FullLife"},
+				TagLimit:         utils.Ptr(float64(15)),
+				TagLimitVariable: utils.Ptr("FullLife"),
+			},
+			multipliers: map[string]float64{"FullLife": 200},
+			expected:    150,
+		},
+	}
+
+	for _, test := range tc {
+		t.Run(test.name, func(t *testing.T) {
+			m := NewModList()
+			for _, tm := range test.storeMods {
+				m.AddMod(tm)
+			}
+			m.Multipliers = test.multipliers
+			got := m.evalMultiplier(test.mod, test.cfg, test.tag)
+			testza.AssertEqual(t, test.expected, got)
+		})
+	}
+}
